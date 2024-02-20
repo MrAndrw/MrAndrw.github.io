dc3a965463dbdba39ab605f913c090e8a373058e
Мальков Андрей <145231986+MrAndrw@users.noreply.github.com>
2024-02-20 03:12:39 +0300

    myinfo

---
+++ myinfo.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let firstname = "Andrey"
+let lastname = "Malkov"
+let city = "Petrozavodsk"
+let age = 21
+let birth = 25.04
+let year = 4
+let uni = "LETI"
+print()
+print("My name is", firstname, lastname)
+print("I am", age, "y.o.")
+print("I was born in", city, "on", birth)
+print("I am studying on a", year,"th year", "in",uni, "university")
